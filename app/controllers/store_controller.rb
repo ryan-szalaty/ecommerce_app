2b9185c0e502a033c332a0dc39500c1eb5adbf0a
ryan-szalaty <ryan.szalaty@gmail.com>
2022-06-21 07:51:27 +0900

    Added error handling on API call.

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,24 +1,36 @@
 class StoreController < ApplicationController
-    def index
-        products = File.open("db/products.json", "r+")
-        products.write("[")
-        JSON.parse(HTTParty.get('https://fakestoreapi.com/products').body).each do |product|
-            JSON.dump({
-                :id => product["id"],
-                :title => product["title"],
-                :price => product["price"],
-                :image => product["image"],
-                :category => product["category"],
-                :description => product["description"]
-            }, products)
-            products.write(",") unless product["id"] == 20
-        end
-        products.write("]")
-        products.close
-        @data = JSON.load(File.open("db/products.json", "r"))
+  def index
+    products = File.open("db/products.json", "r+")
+    products.write("[")
+    begin
+      api_call = JSON.parse(HTTParty.get("https://fakestoreapi.com/products").body)
+    rescue
+      @error = "ERROR"
     end
-    def show
-        @id = params[:id]
-        @product = JSON.load(File.open("db/products.json", "r"))[@id.to_i - 1]
+    unless @error
+      api_call.each do |product|
+        JSON.dump({
+          :id => product["id"],
+          :title => product["title"],
+          :price => product["price"],
+          :image => product["image"],
+          :category => product["category"],
+          :description => product["description"],
+        }, products)
+        products.write(",") unless product["id"] == 20
+      end
+      products.write("]")
+      products.close
     end
+    if @error
+      @data = @error
+    else
+      @data = JSON.load(File.open("db/products.json", "r"))
+    end
+  end
+
+  def show
+    @id = params[:id]
+    @product = JSON.load(File.open("db/products.json", "r"))[@id.to_i - 1]
+  end
 end
